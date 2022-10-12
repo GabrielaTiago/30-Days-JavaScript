d40a854873cc84b8f74ec77f50c80f378523f838
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-12 00:12:34 -0300

    :recycle: refactor: Function do handle the key press sound

--- Drum Kit/js/scripts.js
+++ Drum Kit/js/scripts.js
@@ -1,26 +1,35 @@
-function playAudio(event) {
-    const audio = this.document.querySelector(
-      `audio[data-key="${event.keyCode}"`
-    );
-  
-    const playing = this.document.querySelector(
-      `.key[data-key="${event.keyCode}`
-    );
-  
-    if (!audio) return;
-  
-    audio.currentTime = 0;
-    audio.play();
-  
-    playing.classList.add("playing");
+function playAudio(event, type) {
+  const code = keyOrClick(event, type);
+
+  const audio = this.document.querySelector(`audio[data-key="${code}"`);
+
+  const playing = this.document.querySelector(`.key[data-key="${code}`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+
+  playing.classList.add("playing");
+}
+
+function keyOrClick(event, type) {
+  if (type === "press") return keySound(event);
+}
+
+function keySound(event) {
+  return event.keyCode;
 }
 
 function removeTransition(event) {
-    if (event.propertyName !== "transform") return;
-    this.classList.remove("playing");
+  if (event.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
-window.addEventListener("keydown", playAudio);
+window.addEventListener("keydown", (event) => {
+  const type = "press";
+  playAudio(event, type);
+});
