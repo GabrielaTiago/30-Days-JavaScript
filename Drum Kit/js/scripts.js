c53971ee3be15d588b3c0ffdbcaa6bd20b4be206
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-11 23:56:04 -0300

    :recycle: refactor: Separetes the playAudio funtion from the eventListener

--- Drum Kit/js/scripts.js
+++ Drum Kit/js/scripts.js
@@ -1,24 +1,26 @@
-window.addEventListener("keydown", function (event) {
-  const audio = this.document.querySelector(
-    `audio[data-key="${event.keyCode}"`
-  );
-
-  const playing = this.document.querySelector(
-    `.key[data-key="${event.keyCode}`
-  );
-
-  if (!audio) return;
-
-  audio.currentTime = 0;
-  audio.play();
-
-  playing.classList.add("playing");
-});
+function playAudio(event) {
+    const audio = this.document.querySelector(
+      `audio[data-key="${event.keyCode}"`
+    );
+  
+    const playing = this.document.querySelector(
+      `.key[data-key="${event.keyCode}`
+    );
+  
+    if (!audio) return;
+  
+    audio.currentTime = 0;
+    audio.play();
+  
+    playing.classList.add("playing");
+}
 
 function removeTransition(event) {
     if (event.propertyName !== "transform") return;
     this.classList.remove("playing");
-  }
-  
-  const keys = document.querySelectorAll(".key");
-  keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playAudio);
