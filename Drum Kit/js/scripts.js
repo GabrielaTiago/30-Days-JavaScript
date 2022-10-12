d0c73e20429644ca104b8b0a6b4163166dd542eb
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-11 23:48:36 -0300

    :sparkles: feat: Creates the animation when the key is pressed

--- Drum Kit/js/scripts.js
+++ Drum Kit/js/scripts.js
@@ -3,8 +3,14 @@ window.addEventListener("keydown", function (event) {
     `audio[data-key="${event.keyCode}"`
   );
 
+  const playing = this.document.querySelector(
+    `.key[data-key="${event.keyCode}`
+  );
+
   if (!audio) return;
 
   audio.currentTime = 0;
   audio.play();
+
+  playing.classList.add("playing");
 });
