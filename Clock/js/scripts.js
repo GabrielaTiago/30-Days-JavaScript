425415bf0fef8814eeb8dfc10defbc254d2f046c
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-10-13 13:17:49 -0300

    :sparkles: feat: Creates the js function to rotate the minute hand

--- Clock/js/scripts.js
+++ Clock/js/scripts.js
@@ -1,11 +1,16 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
 
 function setDate() {
   const date = new Date();
   const seconds = date.getSeconds();
+  const minutes = date.getMinutes();
 
   const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
